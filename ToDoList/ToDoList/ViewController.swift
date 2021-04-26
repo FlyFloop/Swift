07beb425a2485b56d3742f497bbc3ed53b49bad0
Alper <alperyorgun2@gmail.com>
2021-04-26 10:58:56 +0300

    tableview done

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    var list : [String] = ["Ekmek al.","Çöpü çıkar.","Faturaları öde."]
+    
 
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
 
 
