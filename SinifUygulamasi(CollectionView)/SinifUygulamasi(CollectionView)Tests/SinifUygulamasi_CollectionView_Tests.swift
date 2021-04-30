62a62f01a229a1659ef8b01288e598e6e5fca706
Alper <alperyorgun2@gmail.com>
2021-04-30 13:05:09 +0300

    zzz

---
+++ SinifUygulamasi(CollectionView)/SinifUygulamasi(CollectionView)Tests/SinifUygulamasi_CollectionView_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  SinifUygulamasi_CollectionView_Tests.swift
+//  SinifUygulamasi(CollectionView)Tests
+//
+//  Created by Alper Yorgun on 30.04.2021.
+//
+
+import XCTest
+@testable import SinifUygulamasi_CollectionView_
+
+class SinifUygulamasi_CollectionView_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
